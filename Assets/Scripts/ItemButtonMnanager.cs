10a352f9264fa75395664bfc83e0cc76245af526
deckoner <deckonerDev@gmail.com>
2025-01-16 20:10:12 +0100

    Modelos y tal

---
+++ Assets/Scripts/ItemButtonMnanager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class ItemButtonMnanager : MonoBehaviour
+{
+    private string ItemName;
+    private Sprite ItemImage;
+    private string ItemDescripcion;
+    private GameObject Item3DModel;
+    [SerializeField] private GameObject sueloSombras;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
